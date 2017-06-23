7e4993abdb8d68d14a142b00fa073d8c459f3d00
Robin Ward <robin.ward@gmail.com>
2017-06-23 14:50:34 -0400

    FIX: spec_helper mixed spaces and tabs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-
 module FileHelpers
 	def file_for(name)
 		File.expand_path("../fixtures/#{name}.json", __FILE__)
@@ -6,15 +5,15 @@ module FileHelpers
 end
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
+	config.expect_with :rspec do |expectations|
+		expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+	end
 
 	config.include FileHelpers
 
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
+	config.mock_with :rspec do |mocks|
+		mocks.verify_partial_doubles = true
+	end
 
-  config.shared_context_metadata_behavior = :apply_to_host_groups
+	config.shared_context_metadata_behavior = :apply_to_host_groups
 end
