discourse/mail-receiver

commit 589222a6b10779746552360f3ae51645c69de495

FEATURE: Allow sender domains to be blacklisted
Under some circumstances, this feature should help prevent messages from
cluttering the Discourse mail logs for admins.

Implement this functionality in `FastRejection`, not Postfix itself, as
that is probably the first place someone is going to look.

lib/mail_receiver/fast_rejection.rb
@@ -4,6 +4,7 @@ require 'uri'
 require 'cgi'
 require 'net/http'
 
+require_relative 'mail'
 require_relative 'mail_receiver_base'
 
 class FastRejection < MailReceiverBase
@@ -12,6 +13,10 @@ class FastRejection < MailReceiverBase
 		super(env_file)
 
 		@disabled = @env['DISCOURSE_FAST_REJECTION_DISABLED'] || !@env['DISCOURSE_BASE_URL']
+
+		@blacklisted_sender_domains = Hash[
+			@env.fetch('BLACKLISTED_SENDER_DOMAINS', "").split(" ").map { |v| [v.downcase, nil] }
+		]
 	end
 
 	def disabled?
@@ -48,6 +53,16 @@ class FastRejection < MailReceiverBase
 			return 'defer_if_permit No sender specified'
 		elsif args['recipient'].nil?
 			return 'defer_if_permit No recipient specified'
+		end
+
+		domain = domain_from_addrspec(args['sender'])
+		if domain.empty?
+			logger.info("deferred mail with domainless sender #{args['sender']}")
+			return 'defer_if_permit Invalid sender'
+		end
+		if @blacklisted_sender_domains.key? domain
+			logger.info("rejected mail from blacklisted sender domain #{domain} (from #{args['sender']})")
+			return 'reject Invalid sender'
 		end
 
 		maybe_reject_email(args['sender'], args['recipient'])

spec/lib/fast_reject_spec.rb
@@ -55,6 +55,36 @@ describe FastRejection do
 			expect(response).to start_with('defer_if_permit')
 		end
 
+		it "returns defer_if_permit if sender addr-spec contains no domain" do
+			response = receiver.process_single_request(
+				'request' => 'smtpd_access_policy',
+				'protocol_state' => 'RCPT',
+				'sender' => 'miscreant',
+				'recipient' => 'discourse@example.com'
+			)
+			expect(response).to start_with('defer_if_permit')
+		end
+
+		it "returns reject if sender domain is blacklisted" do
+			response = receiver.process_single_request(
+				'request' => 'smtpd_access_policy',
+				'protocol_state' => 'RCPT',
+				'sender' => 'miscreant@bad.com',
+				'recipient' => 'discourse@example.com'
+			)
+			expect(response).to start_with('reject')
+		end
+
+		it "returns reject if sender domain is blacklisted with differing case" do
+			response = receiver.process_single_request(
+				'request' => 'smtpd_access_policy',
+				'protocol_state' => 'RCPT',
+				'sender' => 'miscreant@SaD.NeT',
+				'recipient' => 'discourse@example.com'
+			)
+			expect(response).to start_with('reject')
+		end
+
 		it "returns dunno if everything looks good" do
 			expect_any_instance_of(Net::HTTP).to receive(:request) do |http|
 				response = Net::HTTPSuccess.new(http, 200, "OK")

spec/lib/mail_spec.rb
@@ -1,0 +1,13 @@
+require_relative '../../lib/mail_receiver/mail'
+
+RSpec.describe 'domain_from_addrspec' do
+
+	it "normalises domains to lowercase" do
+		expect(domain_from_addrspec("local-part@DOMAIN.NET")).to eq "domain.net"
+	end
+
+	it "returns an empty string if a domain was not found" do
+		expect(domain_from_addrspec("local-part")).to be_empty
+	end
+
+end

lib/mail_receiver/mail.rb
@@ -1,0 +1,10 @@
+# Parse and return the domain from an Internet addr-spec.  Return value is
+# normalised to lowercase.
+#
+# This implementation is the simplest thing that could possibly work.  Do
+# not use this function to parse generalised mailbox or group addresses.
+#
+# See section 3.4 of RFC 2822.
+def domain_from_addrspec(addrspec)
+	(addrspec.split("@", 2)[1] or "").downcase
+end

commit fcd28fb95f581ca227b60661491cca7187218246

FIX: Allow mail from the null sender
Fixes a regression introduced in commit 589222a.

lib/mail_receiver/fast_rejection.rb
@@ -49,33 +49,51 @@ class FastRejection < MailReceiverBase
 		elsif args['protocol_state'] != 'RCPT'
 			return 'dunno'
 		elsif args['sender'].nil?
+			# Note that while this key should always exist, its value may be the empty
+			# string.  Postfix will convert the "<>" null sender to "".
 			return 'defer_if_permit No sender specified'
 		elsif args['recipient'].nil?
 			return 'defer_if_permit No recipient specified'
 		end
 
-		domain = domain_from_addrspec(args['sender'])
+		run_filters(args)
+	end
+
+	def run_filters(args)
+		filters = [
+			:maybe_reject_by_sender_domain,
+			:maybe_reject_by_api,
+		]
+
+		for f in filters do
+			action = send(f, args)
+			break unless action == "dunno"
+		end
+		action
+	end
+
+	def maybe_reject_by_sender_domain(args)
+		sender = args['sender']
+
+		return "dunno" if sender.empty?
+
+		domain = domain_from_addrspec(sender)
 		if domain.empty?
-			logger.info("deferred mail with domainless sender #{args['sender']}")
+			logger.info("deferred mail with domainless sender #{sender}")
 			return 'defer_if_permit Invalid sender'
 		end
 		if @blacklisted_sender_domains.include? domain
-			logger.info("rejected mail from blacklisted sender domain #{domain} (from #{args['sender']})")
+			logger.info("rejected mail from blacklisted sender domain #{domain} (from #{sender})")
 			return 'reject Invalid sender'
 		end
 
-		maybe_reject_email(args['sender'], args['recipient'])
+		return "dunno"
 	end
 
-	def endpoint
-		"#{@env['DISCOURSE_BASE_URL']}/admin/email/smtp_should_reject.json"
-	end
-
-	def maybe_reject_email(from, to)
-
+	def maybe_reject_by_api(args)
 		uri = URI.parse(endpoint)
-		fromarg = CGI::escape(from)
-		toarg = CGI::escape(to)
+		fromarg = CGI::escape(args['sender'])
+		toarg = CGI::escape(args['recipient'])
 
 		api_qs = "api_key=#{key}&api_username=#{username}&from=#{fromarg}&to=#{toarg}"
 		if uri.query and !uri.query.empty?
@@ -108,6 +126,10 @@ class FastRejection < MailReceiverBase
 		end
 
 		return "dunno"  # let future tests also be allowed to reject this one.
+	end
+
+	def endpoint
+		"#{@env['DISCOURSE_BASE_URL']}/admin/email/smtp_should_reject.json"
 	end
 
 end

spec/lib/fast_reject_spec.rb
@@ -41,9 +41,25 @@ describe FastRejection do
 		it "returns defer_if_permit if no sender" do
 			response = receiver.process_single_request(
 				'request' => 'smtpd_access_policy',
-				'protocol_state' => 'RCPT'
+				'protocol_state' => 'RCPT',
+				'recipient' => 'discourse@example.com'
 			)
 			expect(response).to start_with('defer_if_permit')
+		end
+
+		it "returns dunno if from the null sender" do
+			expect_any_instance_of(Net::HTTP).to receive(:request) do |http|
+				response = Net::HTTPSuccess.new(http, 200, "OK")
+				expect(response).to receive(:body) { "{}" }
+				response
+			end
+			response = receiver.process_single_request(
+				'request' => 'smtpd_access_policy',
+				'protocol_state' => 'RCPT',
+				'sender' => '',
+				'recipient' => 'discourse@example.com'
+			)
+			expect(response).to eq('dunno')
 		end
 
 		it "returns defer_if_permit if no recipient" do

spec/lib/mail_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'domain_from_addrspec' do
 		expect(domain_from_addrspec("local-part@DOMAIN.NET")).to eq "domain.net"
 	end
 
+	it "returns an empty string when given an empty string" do
+		expect(domain_from_addrspec("")).to be_empty
+	end
+
 	it "returns an empty string if a domain was not found" do
 		expect(domain_from_addrspec("local-part")).to be_empty
 	end